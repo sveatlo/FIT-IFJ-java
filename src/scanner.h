616586f5d918a8b504affbadff3d4fea5eedf7a0
Svatopluk Hanzel <svatopluk@bobcode.eu>
2016-10-05 14:49:21 +0200

    initial scanner commit

---
+++ src/scanner.h
@@ -0,0 +1,29 @@
+/**
+ *  @defgroup Scanner
+ *  @brief Module responsible for scanning input file.
+ */
+
+#ifndef SCANNER_H
+#define SCANNER_H
+
+/**
+ *  Enumeration of all tokens the scanner may return
+ *
+ * @ingroup Scanner
+ */
+typedef enum {
+
+} ScannerToken;
+
+/**
+ *  Enumeration of all states the scanner can be in
+ *
+ * @ingroup Scanner
+ */
+typedef enum {
+
+} ScannerState;
+
+
+
+#endif
