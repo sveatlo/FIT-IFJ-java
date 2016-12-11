a04ad3c9a2150bafb33b0cda5953ea7c7fa30afc
Svatopluk Hanzel <svatopluk@bobcode.eu>
2016-12-11 19:19:04 +0100

    added header comments

--- src/scanner.c
+++ src/scanner.c
@@ -1,3 +1,18 @@
+/*
+ * Course: IFJ
+ * Project name: Implementace interpretu imperativního jazyka IFJ16
+ * Description: https://wis.fit.vutbr.cz/FIT/st/course-files-st.php/course/IFJ-IT/projects/ifj2016.pdf
+ *
+ *
+ * Team:
+ *  Dominik Križka   (xkrizk02)
+ *  Samuel Hulla     (xhulla00)
+ *  Svätopluk Hanzel (xhanze10)
+ *  Tomáš Haas       (xhaast00)
+ */
+
+
+
 #include <stdlib.h>
 #include <errno.h>
 #include <limits.h>
